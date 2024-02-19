590b247730e5efeee5f31423886efd2483a5375c
Amit Kumar <65074722+Amitkumrsingh@users.noreply.github.com>
2024-02-19 18:35:58 +0000

    Added new api url

--- src/config/conf_local.js
+++ src/config/conf_local.js
@@ -3,7 +3,7 @@ import defaultConfig from './default';
 const localConfig = {
     ...defaultConfig,
 
-    API_BASE_URL: "https://api.ifnotnowthenwhen.in/api/v1",
+    API_BASE_URL: "https://api.youdot.in/api/v1",
 
 
     //API_BASE_URL: "http://127.0.0.1:8086/api/v1"
